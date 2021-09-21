47860d5fb5c35f05a7468dff6730193e53eba0b1
Дмитрий Мунгалов <d.mungalov@gmail.com>
2021-09-21 12:29:13 +0700

    add 53

---
+++ solutions/53_Maximum_Subarray.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// 72 ms
+// var maxSubArray = function (nums) {
+//   let best = nums[0];
+//   let sum = nums[0];
+
+//   for (let i = 1; i < nums.length; i++) {
+//     const num = nums[i];
+
+//     if (num > sum + num) {
+//       sum = num;
+//       best = Math.max(best, sum, num);
+//     } else {
+//       sum = sum + num;
+//       best = Math.max(sum, best);
+//     }
+//   }
+
+//   return best;
+// };
+
+// 64 ms faster than 99.40%
+var maxSubArray = function (nums) {
+  let best = nums[0];
+  let sum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    const num = nums[i];
+
+    sum = Math.max(num, sum + num);
+    best = Math.max(best, sum, num);
+  }
+
+  return best;
+};
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // [4,-1,2,1] => 6
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 6])); // [4,-1,2,1,-5,6] => 7
+console.log(maxSubArray([1])); // [1] => 1
+console.log(maxSubArray([5, 4, -1, 7, 8])); // [5,4,-1,7,8] => 23
+console.log(maxSubArray([-1, -2])); // [-1] => -1
